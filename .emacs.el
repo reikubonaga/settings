3bda6df7b94971a736dd70db17786b4a9f4a939d
rei kubonaga <kubonagarei@gmail.com>
2013-04-20 20:34:35 +0900

    Use pbcopy when only mac

--- .emacs.el
+++ .emacs.el
@@ -48,17 +48,18 @@
 (global-set-key "\C-^" 'other-window)
 
 ;;copy
-(defun copy-from-osx ()
- (shell-command-to-string "pbpaste"))
+(when (eq window-system 'mac)
+ (defun copy-from-osx ()
+  (shell-command-to-string "pbpaste"))
 
-(defun paste-to-osx (text &optional push)
- (let ((process-connection-type nil))
+ (defun paste-to-osx (text &optional push)
+  (let ((process-connection-type nil))
      (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
        (process-send-string proc text)
        (process-send-eof proc))))
 
-(setq interprogram-cut-function 'paste-to-osx)
-(setq interprogram-paste-function 'copy-from-osx)
+ (setq interprogram-cut-function 'paste-to-osx)
+ (setq interprogram-paste-function 'copy-from-osx))
 
 (global-font-lock-mode t)
 
